54a961526f964c8fe1d6dc8b3a1f4f5389502615
ASBcep <asaboritb2324@politecnics.barcelona>
2023-11-24 18:05:55 +0100

    Aplicació funcional, exercici acabat

--- app/src/main/java/asb/m08/m08_uf1_14_login/Usuari.kt
+++ app/src/main/java/asb/m08/m08_uf1_14_login/Usuari.kt
@@ -2,4 +2,4 @@ package asb.m08.m08_uf1_14_login
 
 import java.io.Serializable
 
-data class Usuari (val username: String, val password: String, val nomSencer: String): Serializable
+class Usuari (val username: String, val password: String, val nomSencer: String): Serializable
