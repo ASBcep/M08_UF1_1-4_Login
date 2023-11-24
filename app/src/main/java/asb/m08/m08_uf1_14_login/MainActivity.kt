151493f9615e766630c1346a48aa10592ca311c1
ASBcep <asaboritb2324@politecnics.barcelona>
2023-11-24 15:31:37 +0100

    Initial commit

---
+++ app/src/main/java/asb/m08/m08_uf1_14_login/MainActivity.kt
@@ -0,0 +1,11 @@
+package asb.m08.m08_uf1_14_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M08 UF1 1.4 Login"
+include(":app")
